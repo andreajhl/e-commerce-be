2d7a7908c84c91905dfb144b38b2db27b774f9cd
andreajhl <andreajhl29@gmial.com>
2021-09-08 18:48:56 -0300

    cambios

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { dbConnection } = require("../src/configDB/config");
+const { dbConnection } = require("./src/configDB/config");
 
 const app = express();
 
 dbConnection();
 
-const routes = require("../src/routes/index");
+const routes = require("./src/routes/index");
 
 app.use(cors());
 
